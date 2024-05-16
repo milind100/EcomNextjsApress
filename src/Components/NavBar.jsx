d09e5de2000d4d5b709ecdc2261eab24b6a8159b
Milind <mlptaglineinfotech.com>
2024-05-16 11:24:01 +0530

    removed extra files

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,9 +2,28 @@ import React from "react";
 import { AiOutlineShopping } from "react-icons/ai";
 
 import Cart from "./Cart";
-// const { showCart, setShowCart, totalQuantities } = useStateContext();
+import Link from "next/link";
+// const { showCart, setShowCart, totalQuantities } =useStateContext();
 const NavBar = () => {
-  return div.navbar - container;
+  return (
+    <div className="navbar-container">
+      <div className="company-name">
+        <Link href="/">Mangez Macaron</Link>
+        <div className="navbar" logo="true">
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/shop">Shop</Link>
+          <Link href="/contact">Contact</Link>
+
+          <button className="cart-icon" onClick={() => setShowCart(true)}>
+            <AiOutlineShopping />
+            <span className="cart-item-qty">{/* {totalQuantities} */}</span>
+          </button>
+          {/* {showCart && <Cart />} */}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
